e8d7765614406f158e5390d0a1bb693c0057b282
sj-bl <sj.gamersnation@gmail.com>
2020-06-12 19:33:20 +0530

    homepage is created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./homepage.style.scss";
+const Homepage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Homepage;
